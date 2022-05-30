1a611ad8e163fb00c307c5082732c184189e2f38
alexekorn <alex@symplany.com>
2022-05-29 21:59:23 -0500

    Make a basic Card class and a simple test

---
+++ src/main/kotlin/com/alexkorn/Card.kt
@@ -0,0 +1,34 @@
+package com.alexkorn
+
+class Card(private val rank: Int, private val suit: String) {
+    object CardConstants {
+        const val SUIT_CLUBS = "clubs"
+        const val SUIT_DIAMONDS = "diamonds"
+        const val SUIT_HEARTS = "hearts"
+        const val SUIT_SPADES = "spades"
+    }
+
+    public fun render(): String {
+        val rankMap = HashMap<Int, String>()
+        rankMap[1] = "Ace";
+        rankMap[11] = "Jack";
+        rankMap[12] = "Queen";
+        rankMap[13] = "King";
+        val suitMap = HashMap<String, String>();
+        suitMap[CardConstants.SUIT_CLUBS] = "Clubs";
+        suitMap[CardConstants.SUIT_DIAMONDS] = "Diamonds";
+        suitMap[CardConstants.SUIT_HEARTS] = "Hearts";
+        suitMap[CardConstants.SUIT_SPADES] = "Spades";
+        var result = "";
+        if (rankMap.containsKey(rank)) {
+            result = result.plus(rankMap[rank]);
+        }
+        else {
+            result = result.plus(rank);
+        }
+        result = result.plus(" of ");
+        result = result.plus(suitMap[suit]);
+        return result;
+    }
+
+}
---
+++ src/test/kotlin/com/alexkorn/CardTest.kt
@@ -0,0 +1,13 @@
+package com.alexkorn
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class CardTest {
+    @Test
+    internal fun testRender() {
+        val card = Card(1, Card.CardConstants.SUIT_CLUBS);
+        val result = card.render();
+        assertEquals("Ace of Clubs", result);
+    }
+}
